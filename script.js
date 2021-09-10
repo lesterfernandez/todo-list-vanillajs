fb6225126295c7120a820726321fd4964371c14d
lester <5958256+lesterfernandez@users.noreply.github.com>
2021-09-10 18:56:27 -0400

    Started JS

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const todoInput = document.querySelector(".todo-input");
 const todoSubmit = document.querySelector(".todo-submit");
 const todoList = document.querySelector(".todo-list");
 
+todoSubmit.addEventListener("click", addTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+}
